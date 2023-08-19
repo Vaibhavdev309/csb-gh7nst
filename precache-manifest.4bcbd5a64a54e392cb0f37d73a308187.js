07be401b02c3beb3dd135b71135d896cc345d055
Code Sandbox <hello@codesandbox.io>
2023-08-19 05:42:14 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.4bcbd5a64a54e392cb0f37d73a308187.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7599bf86b9eb8ad05656a5edecd06dcf",
+    "url": "https://vaibhavdev309.github.io/csb-gh7nst/index.html"
+  },
+  {
+    "revision": "ee9e355d3438769b7034",
+    "url": "https://vaibhavdev309.github.io/csb-gh7nst/static/js/2.88831c34.chunk.js"
+  },
+  {
+    "revision": "0468d4fad5f132c609d4",
+    "url": "https://vaibhavdev309.github.io/csb-gh7nst/static/js/main.2f1a1aca.chunk.js"
+  },
+  {
+    "revision": "d3157ed52ca0d4bedb52",
+    "url": "https://vaibhavdev309.github.io/csb-gh7nst/static/js/runtime-main.ccec4071.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "https://vaibhavdev309.github.io/csb-gh7nst/precache-manifest.4bcbd5a64a54e392cb0f37d73a308187.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://vaibhavdev309.github.io/csb-gh7nst/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
